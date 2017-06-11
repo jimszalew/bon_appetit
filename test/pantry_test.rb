952375b14a8e9291efae7f4c0c146a81b9ad11eb
Jim Szalewski <jim.szalewski@gmail.com>
2017-06-11 13:39:01 -0600

    change name and function of previous test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -114,7 +114,7 @@ class PantryTest < Minitest::Test
     assert_equal expected, actual
   end
 
-  def test_it_can_recommend_recipes_based_on_whats_in_stock
+  def test_it_can_add_recipes_to_cookbook
     pantry = Pantry.new
 
     r1 = Recipe.new("Cheese Pizza")
@@ -142,7 +142,39 @@ class PantryTest < Minitest::Test
 
     actual = pantry.cookbook
     expected ={"Cheese Pizza"=>{"Cheese"=>20, "Flour"=>20},  "Pickles"=>{"Brine"=>10, "Cucumbers"=>30}, "Peanuts"=>{"Raw nuts"=>10, "Salt"=>10}}
-# binding.pry
+
     assert_equal actual, expected
+  end
+
+  def test_it_knows_what_recipes_you_can_make_based_on_available_ingredients
+    pantry = Pantry.new
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Pickles", 40)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    actual = pantry.what_can_i_make
+    expected = ["Pickles", "Peanuts"]
+
+    assert_equal expected, actual
   end
 end
